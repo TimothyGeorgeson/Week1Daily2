0a002e0cf17dd4203afee16c8715cebe280628bb
Timothy Georgeson <timothy.georgeson@mobileappscompany.us>
2018-10-23 15:55:36 -0400

    Updating first day code

--- app/src/main/java/com/example/user/firstdaycoding/FirstDayCoding.java
+++ app/src/main/java/com/example/user/firstdaycoding/FirstDayCoding.java
@@ -4,10 +4,12 @@ import java.util.*;
 public class FirstDayCoding {
     public static void main(String[] args) {
 
-        List<String> input = Arrays.asList("cat", "dog", "pig", "cat");
+        List<String> input = Arrays.asList("cat", "cat", "pig", "cat");
 
         findDuplicates(input);
         System.out.println(checkPalindrome("racecar"));
+        fizzBuzz(20);
+        System.out.println(checkAnagram("COAT", "TACO"));
 
     }
 
@@ -38,8 +40,55 @@ public class FirstDayCoding {
 
     public static void fizzBuzz(int count)
     {
+        for (int i=1; i<=count; i++)
+        {
+            if (i%3 == 0)
+            {
+                if (i%5 == 0)
+                {
+                    System.out.println("fizzbuzz");
+                }
+                else
+                {
+                    System.out.println("fizz");
+                }
+            }
+            else if(i%5 == 0)
+            {
+                System.out.println("buzz");
+            }
+            else
+            {
+                System.out.println(i);
+            }
+
+        }
+    }
+
+    public static boolean checkAnagram(String word1, String word2)
+    {
+        char[] chars1 = word1.toCharArray();
+        char[] chars2 = word2.toCharArray();
+        Arrays.sort(chars1);
+        Arrays.sort(chars2);
+        String sorted1 = new String(chars1);
+        String sorted2 = new String(chars2);
+
+
+        if (sorted1.equals(sorted2))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public void printTables(){
 
     }
+
 }
 
 //Week 1 Daily 2
@@ -63,7 +112,7 @@ public class FirstDayCoding {
 //Problem 4
 //Create a function to check if the two strings are Anagrams
 //Eg COAT and TACO would be anagrams
-//public boolean findDuplicates(String word1, String word2){};
+//public boolean findAnagram(String word1, String word2){};
 
 //Problem 5
 //Print a multiplication table from 1 to 10 using multidimensional array
